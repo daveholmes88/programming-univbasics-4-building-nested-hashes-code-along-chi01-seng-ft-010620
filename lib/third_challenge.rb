d312d0f2a2d422e8a558352be39ab68a66dd5493
David Holmes <phoenix1988@gmail.com>
2019-12-23 14:15:54 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -12,6 +12,7 @@ def third_challenge
         :hero => {
         name: "Romeo",
          age: "15"
+         status: "alive"
       },
         },
    :capulet=> {
@@ -26,6 +27,7 @@ def third_challenge
     heroine: {
     name: "Juliet",
     age: "15",
+    status: "alive"
   }
    }
   }
